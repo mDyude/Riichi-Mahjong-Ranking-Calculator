0dc2ae4e7d532cd51a28c81bc5a1d6ad62760905
Huiliang Xia <hxia47@uwo.ca>
2023-11-13 01:10:03 -0500

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CircularProgress from "@mui/material/CircularProgress";
 import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
 import Button from "@mui/material/Button";
 import {
   Add,
